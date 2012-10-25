59bfc249472917f0b0fcbe4a700404f8f7276634
James McPherson <jemcphe@fullsail.edu>
2012-10-25 01:12:48 -0500

    pitchWedge.h & .m … new data member… solidContact… solidContact measures how well the ball was struck.  ranges from 1 - 10, 10 being highest… the calculation/manipulation is designed to reduce the yardage 10 yards for each point below a solidContact level of 10.

--- week1/aoc2_wk1/aoc2_wk1/pitchWedge.h
+++ week1/aoc2_wk1/aoc2_wk1/pitchWedge.h
@@ -10,4 +10,6 @@
 
 @interface pitchWedge : baseGolfClub
 
+@property int solidContact;
+
 @end
--- week1/aoc2_wk1/aoc2_wk1/pitchWedge.m
+++ week1/aoc2_wk1/aoc2_wk1/pitchWedge.m
@@ -10,4 +10,29 @@
 
 @implementation pitchWedge
 
+//synthesizing new data member
+@synthesize solidContact;
+
+//establishing init for custom data member values
+-(id) init
+{
+    self = [super init];
+    if (self != nil) {
+        [self setYardsTraveled:140];
+        [self setSolidContact:0];
+        [self setGolfBall: @"Nike 20XI"];
+        [self setSwingSpeed:105];
+    }
+    return self;
+};
+
+//calculation/manipulation method to determine distance of golf ball traveled with selected club
+-(void)calculateBallDistance
+{
+    if (solidContact <= 10 && solidContact >=1) {
+        [self setYardsTraveled: (solidContact * 10) +40];
+    }
+    NSLog(@"Golf Club: Pitching Wedge Golf Ball: %@ Contact (1-10): %i Yards Traveled: %i", self.golfBall, solidContact, self.yardsTraveled);
+}
+
 @end
