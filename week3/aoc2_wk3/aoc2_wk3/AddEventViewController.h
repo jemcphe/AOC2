0bdab29eed7486d5f5cb0fd9f5b85f25e910f740
James McPherson <jemcphe@fullsail.edu>
2012-10-31 21:42:19 -0500

    Creating week 3 project… set up AddEventViewController.h, .m, .xib… Also, full design for how project should look.  Labels, textview, buttons, and date picker all present… next step is functionality.

---
+++ week3/aoc2_wk3/aoc2_wk3/AddEventViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddEventViewController.h
+//  aoc2_wk3
+//
+//  Created by James McPherson on 10/31/12.
+//  Copyright (c) 2012 James McPherson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddEventViewController : UIViewController
+
+@end
---
+++ week3/aoc2_wk3/aoc2_wk3/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  aoc2_wk3
+//
+//  Created by James McPherson on 10/31/12.
+//  Copyright (c) 2012 James McPherson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return YES;
+    }
+}
+
+@end
